34dad2efca8da7f8eff73089f9562005ca2ff887
Maksym Novikov <maksymn@suntechinnovation.com>
2021-12-04 15:41:37 +0200

    variables

--- src/com/java_comeback/Main.java
+++
@@ -1,7 +0,0 @@
-package com.java_comeback;
-
-public class Main {
-  public static void main(String[] args) {
-    // write your code here
-  }
-}
---
+++ src/com/java_comeback/getting_started_programming_in_java/Variables.java
@@ -0,0 +1,14 @@
+package com.java_comeback.getting_started_programming_in_java;
+
+public class Variables {
+  public static void main(String[] args) {
+    // variables assigned with final cannot be modified
+    final int maxStudents = 25;
+
+    // however, we can initialize variable with final modifier
+    // and assign value to it later
+    final int someVariable;
+    int someOtherVariable = 100;
+    someVariable = someOtherVariable;
+  }
+}
