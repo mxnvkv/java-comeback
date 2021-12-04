936e2348ac45c1663e205d80ac176ab403b2b083
Maksym Novikov <maksymn@suntechinnovation.com>
2021-12-04 19:45:55 +0200

    conditional assignment

---
+++ src/com/java_comeback/getting_started_programming_in_java/ConditionalAssignment.java
@@ -0,0 +1,12 @@
+package com.java_comeback.getting_started_programming_in_java;
+
+public class ConditionalAssignment {
+  public static void main(String[] args) {
+
+    int value1 = 7;
+    int value2 = 5;
+    int maxValue = value1 > value2 ? value1 : value2;
+    System.out.println(maxValue); // 7
+
+  }
+}
