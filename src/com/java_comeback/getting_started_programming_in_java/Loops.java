548ff23f6f11bce9c8d31442c1fea57dfde43e2b
Maksym Novikov <maksymn@suntechinnovation.com>
2021-12-05 13:58:41 +0200

    loops

---
+++ src/com/java_comeback/getting_started_programming_in_java/Loops.java
@@ -0,0 +1,36 @@
+package com.java_comeback.getting_started_programming_in_java;
+
+public class Loops {
+  public static void main(String[] args) {
+
+    // *** While loop ***
+
+    int someValue = 4;
+    int factorial = 1;
+
+    while(someValue > 1) {
+      factorial *= someValue;
+      someValue--;
+    }
+
+    System.out.println(factorial); // 24
+
+    // *** Do-while loop ***
+
+    int iVal = 80;
+
+    do {
+      System.out.print(iVal);
+      System.out.print(" * 2 = ");
+      iVal *= 2;
+      System.out.println(iVal);
+    } while (iVal < 25);
+
+    // *** For loop ***
+
+    for (int i = 1; i < 100; i *= 2) {
+      System.out.println(i);
+    }
+
+  }
+}
