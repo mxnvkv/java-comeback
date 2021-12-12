a4a6437f2e31efcc22384d9d77c70185ee610c3c
Maksym Novikov <maksymn@suntechinnovation.com>
2021-12-12 14:42:23 +0200

    create custom exception

---
+++ src/com/java_comeback/handling_exceptions_in_java/InvalidStatementException.java
@@ -0,0 +1,7 @@
+package com.java_comeback.handling_exceptions_in_java;
+
+public class InvalidStatementException extends Exception {
+  public InvalidStatementException(String message) {
+    super(message);
+  }
+}
